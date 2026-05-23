353c8fa728ffc18b69192b563b3bd18e99d7af9d
dilliswararaogalala <dilliswararaogalala@gmail.com>
2026-05-23 12:46:18 +0530

    adds the worker service that is responsible for the reciept generation

--- booking/main.js
+++ booking/main.js
@@ -1,12 +1,17 @@
 import { createApp } from "./src/app.js";
 import { MongoClient } from "mongodb";
+import {createClient} from "redis"
 
 const main = async() => {
   const mongoClient = new MongoClient("mongodb://mongo:27017/hotel-app");
+  const redis = createClient({
+  url: "redis://redis:6379",
+});
   await mongoClient.connect();
+  await redis.connect()
   const db = mongoClient.db("hotel-app");
   const bookingCollection = db.collection("bookings");
-  const app = createApp(bookingCollection);
+  const app = createApp(bookingCollection,redis);
   Deno.serve({port: 3002}, app.fetch)
 }
 
--- booking/src/app.js
+++ booking/src/app.js
@@ -1,6 +1,7 @@
-import { Hono } from "hono";
+import {  Hono } from "hono";
 import { logger } from "hono/logger";
 import { decode, verify } from "jsonwebtoken";
+import { ObjectId } from "mongodb";
 
 const secret = "bheemlaNayakParasuramKaruppuGabbarSinghJerseySalaarDragonKGF";
 
@@ -8,27 +9,33 @@ const bookHotel = async (c) => {
   const { hotelId, rooms } = await c.req.json();
   const bookings = c.get("bookings");
   const username = c.get("username");
+  const redis = c.get("redis");
   const booking = await bookings.insertOne({
     username,
     hotelId,
     rooms,
+    receiptGenerationStatus: "PENDING",
   });
 
-  const res = await fetch("http://search:3001/api/internal/update-rooms",{
-    method :"POST",
-    body:JSON.stringify({
+  await fetch("http://search:3001/api/internal/update-rooms", {
+    method: "POST",
+    body: JSON.stringify({
       hotelId,
-      rooms
+      rooms,
     }),
-    headers:{"content-type" : "application/json"}
-  })
-  
+    headers: { "content-type": "application/json" },
+  });
+  await redis.rPush(
+    "booking_queue",
+    JSON.stringify({ id: booking.insertedId.toString() }),
+  );
 
   return c.json({
     bookingId: booking.insertedId.toString(),
     username,
     hotelId,
     rooms,
+    receiptGenerationStatus: "PENDING",
   });
 };
 
@@ -38,11 +45,27 @@ const getBookings = async (c) => {
   return c.json(allBookings);
 };
 
-export const createApp = (bookings) => {
+const generateReceipt = async (c) => {
+  const id = c.req.param("id");
+  const bookings = c.get("bookings");
+  const bookingData = await bookings.findOne({ _id: new ObjectId(id) });
+  if (bookingData.receiptGenerationStatus === "PENDING") {
+    return c.json({ msg: "Receipt is not yet generated" });
+  }
+  return c.json({
+    bookingId: bookingData._id.toString(),
+    username: bookingData.username,
+    hotelId: bookingData.hotelId,
+    rooms: bookingData.rooms,
+  });
+};
+
+export const createApp = (bookings, redis) => {
   const app = new Hono();
   app.use(logger());
   app.use((c, next) => {
     c.set("bookings", bookings);
+    c.set("redis", redis);
     return next();
   });
 
@@ -66,6 +89,8 @@ export const createApp = (bookings) => {
   app.get("/api/bookings", getBookings);
 
   app.post("/api/bookings", bookHotel);
+
+  app.get("/api/bookings/:id/receipt", generateReceipt);
 
   return app;
 };
